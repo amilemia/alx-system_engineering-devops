fa2a4c97171d20a84ca9c8cf3a5a8284181e7081
Amilemia <aminemadah2@gmail.com>
2023-10-24 03:51:16 +0100

    Fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,6 +19,19 @@ if __name__ == "__main__":
     user = requests.get(url + '/users/{}'.format(user_id)).json()
     todos = requests.get(url + '/users/{}/todos'.format(user_id)).json()
 
+    # Correct output formatting
+    message = ''
+    for task in todos:
+        if task.get('userId') == int(user_id):
+            message += 'Task {}: '.format(task.get('title'))
+            message += str(task.get('completed'))
+            message += '\n'
+
+    # Correct user ID and username retrieved
+    username = user.get('name')
+    if username:
+        message = 'User ID and Username: {}\n'.format(username) + message
+
     with open('{}.csv'.format(user_id), 'w', newline='') as file:
         writer = csv.writer(file, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL, lineterminator='\n')
@@ -26,3 +39,6 @@ if __name__ == "__main__":
             if task.get('userId') == int(user_id):
                 writer.writerow([user_id, user.get('name'),
                                  task.get('completed'), task.get('title')])
+
+    # Print the message
+    print(message)
